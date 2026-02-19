13316c536b23504dbe3c95de25fd39b7c25e7c92
madhih <madhih@proton.me>
2026-02-19 04:31:41 +0000

    connected everything

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes";
 
 const app = express();
 app.use(express.json());
@@ -17,6 +18,7 @@ mongoose.connect(mongodbUri)
         console.error("Error connecting to MongoDB:", error);
     });
 
+app.use('/api/auth', authRoutes);
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
